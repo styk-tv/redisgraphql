8ef23dd32b63edbd1ae00dc9d425ce6e868b6133
Peter Styk <peter@styk.tv>
2019-04-12 10:52:44 +0100

    pushd to redis-flask for gunicorn

--- perf/flask-redis/run.sh
+++ perf/flask-redis/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 #python /flask-redis/main.py
-gunicorn main:redis --workers=10 --timeout=10 --bind=:8080
+
+pushd /flask-redis/
+    gunicorn main:redis --workers=10 --timeout=10 --bind=:8080
+popd
