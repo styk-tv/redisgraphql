5e165177aafdc84f51ddf1e787ef73fd7cebb113
Peter Styk <peter@styk.tv>
2019-04-12 13:14:33 +0100

    gunicorn workers decreased from 35 to 15

--- perf/flask-redis/run.sh
+++ perf/flask-redis/run.sh
@@ -3,5 +3,5 @@
 #python /flask-redis/main.py
 
 pushd /flask-redis/
-    gunicorn main:redis --workers=35 --timeout=10 --bind=:8080
+    gunicorn main:redis --workers=15 --timeout=10 --bind=:8080
 popd
